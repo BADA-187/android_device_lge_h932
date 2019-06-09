4d0a44bacd8d2e866641c73fe822aa43bcff83ed
BADA-187 <bada_@live.nl>
2019-06-09 11:45:16 +0200

    cosp-ify
    Change-Id: I050b8e6d466af6a831a9f037df45d694a15831a2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/bootleg_h932.mk
+    $(LOCAL_DIR)/cosp_h932.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo bootleg_h932-userdebug
+add_lunch_combo cosp_h932-userdebug
